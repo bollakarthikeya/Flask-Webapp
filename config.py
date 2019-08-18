821d5aa7390ea5d4bddc04707664bb9a07b1e073
Bolla Karthikeya <10mse1009@gmail.com>
2019-08-18 12:29:31 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-config = dict()
-config['BUCKET_NAME'] = 'YOUR_BUCKET_NAME'
-config['ACCESS_ID'] = 'YOUR_ACCESS_ID'
-config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
-config['FILE_LOCATION'] = './files/'
-config['APP_SECRET_KEY'] = 'SECRET_KEY_OF_YOUR_CHOICE'
